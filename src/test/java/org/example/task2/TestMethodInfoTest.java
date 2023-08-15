76b1f9bb18b67328a916492d34b644a0f86cb8b4
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-08-15 19:51:36 +0300

    reversed back @Listener(PlaygroundListener.class)

--- src/test/java/org/example/task2/TestMethodInfoTest.java
+++ src/test/java/org/example/task2/TestMethodInfoTest.java
@@ -1,48 +1,30 @@
 package org.example.task2;
 
-import org.junit.jupiter.api.TestInfo;
-import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.ServiceLoader;
-import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-//@Listeners(PlaygroundListener.class)
+@Listeners(PlaygroundListener.class)
 public class TestMethodInfoTest {
     @Test
-    @TestMethodInfo(priority = Priority.Critical, author = "Ellina Obukhova", lastModified = "15.08.2023")
+    @TestMethodInfo(priority = Priority.Critical, author = "Test User", lastModified = "15.08.2023")
     public void annotation() {
-        ServiceLoader<PlaygroundListener> loader = ServiceLoader.load(PlaygroundListener.class);
-        Optional<PlaygroundListener> listener = loader.findFirst();
 
-        ITestResult currentTestResult = getCorrespondingResultFor(testContext.getTestMethod());
+//        ServiceLoader<PlaygroundListener> loader = ServiceLoader.load(PlaygroundListener.class);
+//        Optional<PlaygroundListener> listener = loader.findFirst();
+//        listener.ifPresent(playgroundListener -> playgroundListener.onTestStart(Reporter.getCurrentTestResult().getStatus()));
 
-        listener.ifPresent(playgroundListener -> playgroundListener.onTestStart(currentTestResult));
+        // I'm sorry but i don't understand, why we should introduce ServiceLoader
+        // and get a problem with retrieving test result and test context,
+        // when we can just put an annotation @Listener. I couldn't manage to introduce it properly
+        // so I will leave an annotation there
 
-        int result = Reporter.getCurrentTestResult().getStatus();
         assertEquals(1, 1);
-    }
-
-    private ITestResult getCorrespondingResultFor(Method method) {
-        ITestContext context = Reporter.getCurrentTestResult().getTestContext();
-        Set<ITestResult> allResults = new HashSet<>();
-
-        allResults.addAll(context.getPassedTests().getAllResults());
-        allResults.addAll(context.getFailedTests().getAllResults());
-        allResults.addAll(context.getSkippedTests().getAllResults());
-
-        return allResults
-                .stream()
-                .filter(result -> result.getMethod().getConstructorOrMethod().getMethod().equals(method))
-                .findAny()
-                .orElse(null);
     }
 }
