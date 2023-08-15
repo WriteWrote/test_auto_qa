26bbe80f9bcfc40508c77666742975ce74d69a3e
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-08-15 18:20:51 +0300

    Listener's introduced, annotation parameters're printed. WITHOUT service loader just yet.

--- src/test/java/org/example/task2/MyListener.java
+++ src/test/java/org/example/task2/MyListener.java
@@ -2,18 +2,37 @@ package org.example.task2;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
+import org.testng.annotations.CustomAttribute;
+
+import java.lang.reflect.Method;
 
 public class MyListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult result) {
-//        ITestListener.super.onTestStart(result);
-        System.out.println("!!!! Listener !!!! " + result.getTestName());
+        ITestListener.super.onTestStart(result);
+        System.out.println("Test with name \"" + result.getName() + "\" started");
+
+        Method method = result.getMethod().getConstructorOrMethod().getMethod();        // this is ugly, but so be it
+        TestMethodInfo annotation = method.getAnnotation(TestMethodInfo.class);
+
+        // it would be good to use a smart way to define which parameters should method print,
+        // but I don't know how to do it, so it will be just printed
+
+        System.out.println("\nTestMethodInfo parameters:");
+
+        System.out.println("    Author: " + annotation.author());
+        System.out.println("    Last modified: " + annotation.lastModified());
+        System.out.println("    Priority: " + annotation.priority());
+
+        System.out.println("\n");
     }
 
     @Override
     public void onStart(ITestContext context) {
-//        ITestListener.super.onStart(context);
-        System.out.println("Test start");
+        ITestListener.super.onStart(context);
+        System.out.println("\n" + context.getName() + " started");
     }
+
 }
--- src/test/java/org/example/task2/TestMethodInfoTest.java
+++ src/test/java/org/example/task2/TestMethodInfoTest.java
@@ -14,25 +14,6 @@ public class TestMethodInfoTest {
     @Test
     @TestMethodInfo(priority = Priority.Critical, author = "Test user", lastModified = "20.08.2019")
     public void annotation() {
-        ServiceLoader<TestMethodInfoTest> loader = ServiceLoader.load(TestMethodInfoTest.class);
-//        for (TestMethodInfo t : loader){
-//            System.out.println(t.author());
-//        }
-//        var pngFactories = loader
-//                .stream()                                              // Note a below
-//                .filter(p -> p.type().isAnnotationPresent(TestMethodInfo.class))  // Note b
-//                .map(ServiceLoader.Provider::get)                                    // Note c
-//                .collect(Collectors.toSet());
-
-
-//        for (Method m : TestMethodInfoTest.class.getMethods()) {
-//            TestMethodInfo annotation = m.getAnnotation(TestMethodInfo.class);
-//            if (annotation != null)
-//                for (var a : annotation.getClass().getMethods()) {
-//                    System.out.println(a.toGenericString());
-//                }
-//        }
-
         assertEquals(1, 1);
     }
 }
