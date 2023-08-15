62c5040ffaac2236c3eece0f832a5b95a84c6dfc
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-08-15 16:27:05 +0300

    first try to introduce Listener to class * need to switch in Gradle to useTestNG() instead of useJUnitTest()

--- src/main/java/org/example/task2/TestMethodInfo.java
+++ src/main/java/org/example/task2/TestMethodInfo.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE}) //on class level
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface TestMethodInfo {
     Priority priority() default Priority.Major;
 
--- src/test/java/org/example/task2/TestMethodInfoTest.java
+++ src/test/java/org/example/task2/TestMethodInfoTest.java
@@ -1,18 +1,38 @@
 package org.example.task2;
 
-import org.example.task2.Priority;
-import org.example.task2.TestMethodInfo;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
 import java.util.ServiceLoader;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.assertEquals;
 
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Listeners(MyListener.class)
 public class TestMethodInfoTest {
     @Test
     @TestMethodInfo(priority = Priority.Critical, author = "Test user", lastModified = "20.08.2019")
     public void annotation() {
-        ServiceLoader<TestMethodInfo> loader = ServiceLoader.load(TestMethodInfo.class);
+        ServiceLoader<TestMethodInfoTest> loader = ServiceLoader.load(TestMethodInfoTest.class);
+//        for (TestMethodInfo t : loader){
+//            System.out.println(t.author());
+//        }
+//        var pngFactories = loader
+//                .stream()                                              // Note a below
+//                .filter(p -> p.type().isAnnotationPresent(TestMethodInfo.class))  // Note b
+//                .map(ServiceLoader.Provider::get)                                    // Note c
+//                .collect(Collectors.toSet());
+
+
+//        for (Method m : TestMethodInfoTest.class.getMethods()) {
+//            TestMethodInfo annotation = m.getAnnotation(TestMethodInfo.class);
+//            if (annotation != null)
+//                for (var a : annotation.getClass().getMethods()) {
+//                    System.out.println(a.toGenericString());
+//                }
+//        }
+
         assertEquals(1, 1);
     }
 }
